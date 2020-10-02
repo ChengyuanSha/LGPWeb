2f9dcf9b64aa063813e44bec67d295fce500f0f5
Chengyuan Sha <15cs69@caslab.queensu.ca>
2020-10-02 16:22:39 -0400

    update explanation text

--- app.py
+++ app.py
@@ -103,7 +103,13 @@ app.layout = html.Div(
         html.Div([
             dcc.Markdown(
                 '''
-                ##### Check instructions on github README, you need to run algorithm first then upload the two result files here. 
+                ##### SMILE is a bioinformatic framework that learns interpretable predictive models and identifies key metabolites 
+                ##### and their interactions explaining a complex trait. To use SIMILE, please follow these easy steps.
+                1. Format the input data where rows are samples and columns are the label (first column named category)   
+                and features (metabolite concentrations), data also need to be imputed and normalized
+                2. Download and run the SMILE algorithm. All the source code and documentation can be found 
+                on [Github](https://github.com/ChengyuanSha/linear_genetic_programming).
+                3. Upload the result file and the original data here to visualize and interpret the learning results.
                 '''),
             # pickle file upload
             html.Div([
